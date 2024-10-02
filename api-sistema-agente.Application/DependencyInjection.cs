6c274144011aa6398aa71c3a40cec557f52688eb
Filipe <filipe@evosix.com.br>
2024-10-02 10:33:10 -0300

    feat: implement auth services

---
+++ api-sistema-agente.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using api_sistema_agente.Application.Services;
+using api_sistema_agente.Application.Services.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace api_sistema_agente.Application;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddApplication(this IServiceCollection services)
+  {
+    services.AddScoped<IAuthServices, AuthServices>();
+
+    return services;
+  }
+}
