0895a9a0d9e959764dd72ef1d226d71c59a4fcc2
Filipe <filipe@evosix.com.br>
2024-10-02 10:27:29 -0300

    feat: implement jwt handling service

--- api-sistema-agente.Infrastructure/DependencyInjection.cs
+++ api-sistema-agente.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
     services.AddScoped<IMailService, MailService>();
+    services.AddScoped<ITokenService, TokenService>();
     services.AddScoped<IPasswordEncryption, PasswordEncryption>();
 
     services.Configure<SecretsApi>(configuration.GetSection("SecretsApi"));
@@ -6,3 +7,21 @@
 
     var serviceProvider = services.BuildServiceProvider();
     var secretsOptions = serviceProvider.GetRequiredService<IOptions<SecretsApi>>().Value;
+
+    services.AddAuthentication(x =>
+    {
+      x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+      x.RequireHttpsMetadata = false;
+      x.SaveToken = true;
+      x.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretsOptions.SecretKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+      };
+    });
