9297dc33b4dcddf025e69c25865525c784a3f7d2
Filipe <filipe@evosix.com.br>
2024-10-23 09:56:31 -0300

    feat: implement business logic to entities

--- api-sistema-agente.Domain/Entities/Auth.cs
+++ api-sistema-agente.Domain/Entities/Auth.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace api_sistema_agente.Domain.Entities;
+﻿namespace api_sistema_agente.Domain.Entities;
 
 public partial class Auth
 {
-    public int Id { get; set; }
-    public string? Login { get; set; }
-    public string? Mail { get; set; }
-    public string? Password { get; set; }
+    public int Id { get; private set; }
+    public string? Login { get; private set; }
+    public string? Mail { get; private set; }
+    public string? Password { get; private set; }
+
+    public Auth(string login, string mail, string password)
+    {
+        Login = login;
+        Mail = mail;
+        SetPassword(password);
+    }
+
+    public Auth(string login, string password)
+    {
+        Login = login;
+        Password = password;
+    }
+
+    private static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, Password);
+    }
+
+    public void SetPassword(string password)
+    {
+        Password = HashPassword(password);
+    }
 }
--- api-sistema-agente.Domain/Entities/Pendencie.cs
+++ api-sistema-agente.Domain/Entities/Pendencie.cs
@@ -2,23 +2,36 @@
 
 public partial class Pendencie
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public string IdProtocol { get; set; } = null!;
+    public string IdProtocol { get; private set; } = null!;
 
-    public string Document { get; set; } = null!;
+    public string Document { get; private set; } = null!;
 
-    public string Name { get; set; } = null!;
+    public string Name { get; private set; } = null!;
 
-    public int? IdAgent { get; set; }
+    public int? IdAgent { get; private set; }
 
-    public byte[] DateRegister { get; set; } = null!;
+    public byte[] DateRegister { get; private set; } = null!;
 
-    public string Title { get; set; } = null!;
+    public string Title { get; private set; } = null!;
 
-    public string Description { get; set; } = null!;
+    public string Description { get; private set; } = null!;
 
-    public string ProductName { get; set; } = null!;
+    public string ProductName { get; private set; } = null!;
 
-    public string? AgentName { get; set; }
+    public string? AgentName { get; private set; }
+
+    public Pendencie(string idProtocol, string document, string name, int? idAgent, string title, string description, string productName, string? agentName)
+    {
+        IdProtocol = idProtocol;
+        Document = document;
+        Name = name;
+        IdAgent = idAgent;
+        Title = title;
+        Description = description;
+        ProductName = productName;
+        AgentName = agentName;
+        DateRegister = BitConverter.GetBytes(DateTime.UtcNow.Ticks);
+    }
 }
